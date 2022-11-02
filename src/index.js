70afd70d22fc2f12e37601df0d5c04e09f2c362d
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-11-02 11:38:42 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-import _ from 'lodash';
+const tomake = document.getElementById("firstidoftodo");
 
-function component() {
-    const element = document.createElement('div');
-  
-   // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
+const Listiftodo = [
+  {
+    index: 1,
+    description: "Create a todo list",
+    completed: false,
+  },
+  {
+    index: 2,
+    description: "Create a webpack",
+    completed: false,
+  },
+  {
+    index: 3,
+    description: "Create a redux with react",
+    completed: false,
+  },
+];
+
+function displaytodolist() {
+  let count;
+  for (count = 0; count < Listiftodo.length; count += 1) {
+    tomake.innerHTML += `
+     <div class="containertodo">
+     <div class="backtodo">
+     <input type="checkbox" class="check" name="completed" value="${Listiftodo[count].completed}">
+      <p class="description">${Listiftodo[count].description}</p>
+     </div>
+     <div class="makechange">
+     <p>...</p>
+     </div>
+     </div>
+     `;
   }
-  
-  document.body.appendChild(component());
+}
+
+document.addEventListener("DOMContentLoaded", displaytodolist, false);
